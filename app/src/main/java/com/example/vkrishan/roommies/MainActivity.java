9062628c05907acdb11257c21c809094613e675f
Vaibhav Krishan <vkrishan@vkrishan-macmini.roam.corp.google.com>
2018-07-17 19:14:46 -0700

    New Toolbar, menu, logout button

--- app/src/main/java/com/example/vkrishan/roommies/MainActivity.java
+++ app/src/main/java/com/example/vkrishan/roommies/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -14,7 +16,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private android.support.v7.widget.Toolbar toolbar;
+    private android.support.v7.widget.Toolbar mtoolbar;
     private TabLayout tablayout;
     private ViewPager viewPager;
     private FirebaseAuth mAuth;
@@ -27,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mtoolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mtoolbar);
+        getSupportActionBar().setTitle("Roomiesss");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -48,10 +51,33 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         if(currentUser==null){
-            Intent startIntent = new Intent(MainActivity.this, Welcome.class);
-            startActivity(startIntent);
-            finish(); // so user doesn't come back here on pressing BACK button.
+            sendToLoginPage();
         }
     }
 
+    private void sendToLoginPage(){
+        Intent startIntent = new Intent(MainActivity.this, Welcome.class);
+        startActivity(startIntent);
+        finish(); // so user doesn't come back here on pressing BACK button.
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+         getMenuInflater().inflate(R.menu.main_menu, menu);
+
+         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         super.onOptionsItemSelected(item);
+
+         if(item.getItemId()== R.id.btn_logout){
+             FirebaseAuth.getInstance().signOut();
+             sendToLoginPage();
+         }
+
+         return true;
+    }
 }
