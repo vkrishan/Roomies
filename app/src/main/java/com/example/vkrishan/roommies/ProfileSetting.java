81e77ebe4e7c0322219f911c6c3af28769ef222d
Vaibhav Krishan <vkrishan@vkrishan-macmini.roam.corp.google.com>
2018-07-20 16:59:10 -0700

    Profile page data retrieve

--- app/src/main/java/com/example/vkrishan/roommies/ProfileSetting.java
+++ app/src/main/java/com/example/vkrishan/roommies/ProfileSetting.java
@@ -5,9 +5,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileSetting extends AppCompatActivity {
+
+    private DatabaseReference mUserDatabase;
+    private FirebaseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +28,36 @@ public class ProfileSetting extends AppCompatActivity {
         setContentView(R.layout.activity_profile_setting);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        String curr_uid = mCurrentUser.getUid(); // Gets UID of the user which is our KEY
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(curr_uid); // Database record of 1 user
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+     //             Toast.makeText(getApplicationContext(), "------>>"+dataSnapshot.toString(), Toast.LENGTH_SHORT).show();
+                String u_name = dataSnapshot.child("name").getValue().toString();
+                String u_img = dataSnapshot.child("image").getValue().toString();
+                String u_status = dataSnapshot.child("status").getValue().toString();
+                String u_thumb_image = dataSnapshot.child("thumb_image").getValue().toString();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+
+
+            }
+        });
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
