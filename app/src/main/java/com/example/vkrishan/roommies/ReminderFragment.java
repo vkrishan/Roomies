30b8b82e51059ebd788953b9aabb98dac4d5b0eb
Vaibhav Krishan <vkrishan@vkrishan-macmini.roam.corp.google.com>
2018-12-05 19:47:59 -0800

    Added reminder layout, functionality tbd

--- app/src/main/java/com/example/vkrishan/roommies/ReminderFragment.java
+++ app/src/main/java/com/example/vkrishan/roommies/ReminderFragment.java
@@ -1,12 +1,22 @@
 package com.example.vkrishan.roommies;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TimePicker;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
 
 
 public class ReminderFragment extends Fragment {
@@ -18,6 +28,10 @@ public class ReminderFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    PopupWindow newReminderPopup;
+
+
 
 //    private OnFragmentInteractionListener mListener;
 
@@ -50,13 +64,44 @@ public class ReminderFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reminder, container, false);
+        View view = inflater.inflate(R.layout.fragment_reminder, container, false);
+
+        FloatingActionButton fab2 = (FloatingActionButton) view.findViewById(R.id.fab2);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+             // Call AlertDialog for reminder input
+                // https://developer.android.com/guide/topics/ui/dialogs
+                // https://developer.android.com/guide/topics/ui/controls/pickers
+                // Date and time-> https://www.journaldev.com/9976/android-date-time-picker-dialog
+
+
+
+
+
+
+
+
+            }
+        });
+
+
+        return view;
     }
+
+
 
 }
